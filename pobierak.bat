307c8e1c00b9d4f7f7e955bfebea58502bb5f169
pagend0s <65870887+pagend0s@users.noreply.github.com>
2022-09-10 15:00:25 +0200

    Backup function added
    Backup function added after a failed downloader update.

--- pobierak.bat
+++ pobierak.bat
@@ -8,4 +8,22 @@ echo "STARTING POBIERAK"
 
 powershell -ExecutionPolicy Bypass -File .\resources\pobierak.ps1
 
-EXIT
+IF %ERRORLEVEL% EQU 0 (GOTO EXIT) ELSE (GOTO after_error)
+
+:after_error
+
+echo "STARTING POBIERAK BAKUP"
+
+powershell -ExecutionPolicy Bypass -File .\resources\pobierak_bak.ps1
+
+IF %ERRORLEVEL% EQU 0 (GOTO EXIT) ELSE (GOTO after_error2)
+
+:after_error2
+
+
+echo "STARTING POBIERAK BAKUP PRIMARY"
+
+powershell -ExecutionPolicy Bypass -File .\resources\pobierak_primary.ps1
+
+
+:EXIT
