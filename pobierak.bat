bc8023dbda9be0771f27b8face40b3897728530a
pagend0s <65870887+pagend0s@users.noreply.github.com>
2024-06-02 13:04:47 +0200

    EXIT(3) handling
    EXIT(3) handling

--- pobierak.bat
+++ pobierak.bat
@@ -14,7 +14,6 @@ powershell -WindowStyle Normal -ExecutionPolicy Bypass -File .\resources\pobiera
 
 IF %ERRORLEVEL% == 0 (GOTO EXIT)
 
-
 IF %ERRORLEVEL% == 1 (GOTO after_error)
 
 IF %ERRORLEVEL% == 3 (GOTO after_update) ELSE (GOTO EXIT)
@@ -25,7 +24,11 @@ echo "STARTING POBIERAK BAKUP"
 
 powershell -WindowStyle Maximized -ExecutionPolicy Bypass -File .\resources\pobierak_bak.ps1
 
-IF %ERRORLEVEL% EQU 0 (GOTO EXIT) ELSE (GOTO after_error2)
+IF %ERRORLEVEL% == 0 (GOTO EXIT) 
+
+IF %ERRORLEVEL% == 1 (GOTO after_error2)
+
+IF %ERRORLEVEL% == 3 (GOTO after_update) ELSE (GOTO EXIT)
 
 :after_error2
 
@@ -33,8 +36,18 @@ echo "STARTING POBIERAK BAKUP PRIMARY"
 
 powershell -WindowStyle Maximized -ExecutionPolicy Bypass -File .\resources\pobierak_primary.ps1
 
+IF %ERRORLEVEL% == 0 (GOTO EXIT)
+
+IF %ERRORLEVEL% == 3 (GOTO after_update) ELSE (GOTO EXIT)
+
 :after_update
 
 powershell -WindowStyle Normal -ExecutionPolicy Bypass -File .\resources\pobierak.ps1 2>.\resources\Error\error.txt
+
+IF %ERRORLEVEL% == 0 (GOTO EXIT)
+
+IF %ERRORLEVEL% == 1 (GOTO after_error)
+
+IF %ERRORLEVEL% == 3 (GOTO after_update) ELSE (GOTO EXIT)
 
 :EXIT
